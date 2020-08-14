ccbef2e6812670d3de07d09f960db85f83587e14
hongyeongjune <hongyeongjune@naver.com>
2020-08-14 20:35:16 +0900

    정렬 / 프로그래머스 고득점 킷 / Level1

---
+++ src/Sort/프로그래머스/K번째수/Solution.java
@@ -0,0 +1,20 @@
+package Sort.프로그래머스.K번째수;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        for(int i=0; i<commands.length; i++) {
+            int index = 0;
+            int[] sort = new int[commands[i][1] - commands[i][0]+1];
+            for(int j=commands[i][0]-1; j<commands[i][1]; j++) {
+                sort[index++] = array[j];
+            }
+
+            Arrays.sort(sort);
+            answer[i] = sort[commands[i][2]-1];
+        }
+        return answer;
+    }
+}
