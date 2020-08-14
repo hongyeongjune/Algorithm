2e87f73c9ee333c83bd1ea9344c7f12337d4d422
hongyeongjune <hongyeongjune@naver.com>
2020-08-14 21:56:33 +0900

    정렬 / 프로그래머스 고득점 킷 / Level2

---
+++ src/Sort/프로그래머스/가장큰수/Solution.java
@@ -0,0 +1,32 @@
+package Sort.프로그래머스.가장큰수;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        String[] strings = new String[numbers.length];
+
+        for(int i=0; i<numbers.length; i++) {
+            strings[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(strings, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return (o2+o1).compareTo(o1+o2);
+            }
+        });
+
+        if(strings[0].equals("0")) {
+            answer += "0";
+        } else {
+            for(int i=0; i<strings.length; i++) {
+                answer += strings[i];
+            }
+        }
+
+        return answer;
+    }
+}
