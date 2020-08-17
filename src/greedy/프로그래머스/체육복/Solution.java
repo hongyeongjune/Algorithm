8f6a717a887ae7633789fe2980a6948167882e90
hongyeongjune <hongyeongjune@naver.com>
2020-08-17 16:46:39 +0900

    그리디 알고리즘 / 프로그래머스 고득점 킷 / Level1

---
+++ src/greedy/프로그래머스/체육복/Solution.java
@@ -0,0 +1,36 @@
+package greedy.프로그래머스.체육복;
+
+public class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        int[] numbers = new int[n+2];
+
+        for(int i=0; i<lost.length; i++) {
+            numbers[lost[i]] = 1;
+        }
+
+        for(int i=0; i<reserve.length; i++) {
+
+            if(numbers[reserve[i]] == 1 ) numbers[reserve[i]] = 0;
+            else numbers[reserve[i]] = 2;
+
+        }
+
+        for(int i=1; i<=n; i++) {
+            if(numbers[i] == 0 || numbers[i] == 2)
+                answer++;
+            else if(numbers[i] == 1 && numbers[i-1] == 2 && i-1 != 0) {
+                answer++;
+                numbers[i-1] = 0;
+                numbers[i] = 0;
+            }
+            else if(numbers[i] == 1 && numbers[i+1] == 2 && i+1 != n+1) {
+                answer++;
+                numbers[i+1] = 0;
+                numbers[i] = 0;
+            }
+        }
+
+        return answer;
+    }
+}
