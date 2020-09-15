bb327a8232d0e3ca68d25816f1ecae7bfce98fdd
hongyeongjune <hongyeongjune@naver.com>
2020-09-15 11:36:40 +0900

    월간 코드 챌린지 시즌1 /두 개 뽑아서 더하기 / 프로그래머스 / Level1

---
+++ src/월간코드챌린지/시즌1/두개뽑아서더하기/Solution.java
@@ -0,0 +1,30 @@
+package 월간코드챌린지.시즌1.두개뽑아서더하기;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class Solution {
+    public int[] solution(int[] numbers) {
+        HashSet<Integer> hashSet = new HashSet<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers.length; j++) {
+                if (i != j) {
+                    hashSet.add(numbers[i] + numbers[j]);
+                }
+            }
+        }
+
+        Iterator<Integer> iterator = hashSet.iterator();
+        int[] answer = new int[hashSet.size()];
+        int index = 0;
+        while (iterator.hasNext()) {
+            answer[index] = iterator.next();
+            index++;
+        }
+
+        Arrays.sort(answer);
+        return answer;
+    }
+}
