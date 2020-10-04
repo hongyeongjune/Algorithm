1c814aa63be124068740b342fc9a890dea75bb5c
hongyeongjune <hongyeongjune@naver.com>
2020-10-04 14:49:18 +0900

    다시풀어보기 / 힙 정렬 / 프로그래머스 고득점 킷 / Level2

---
+++ src/HeapSort/프로그래머스/더맵게/Retry.java
@@ -0,0 +1,29 @@
+package HeapSort.프로그래머스.더맵게;
+
+import java.util.PriorityQueue;
+
+public class Retry {
+    public static int solution(int[] scoville, int K) {
+
+        int answer = 0;
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+
+        for (int s : scoville) {
+            priorityQueue.offer(s);
+        }
+
+        while (priorityQueue.size() > 1 && priorityQueue.peek() < K) {
+            int first = priorityQueue.poll();
+            int second = priorityQueue.poll() * 2;
+            priorityQueue.offer(first + second);
+            answer++;
+        }
+
+        if (priorityQueue.size() == 1 && priorityQueue.peek() < K)
+            return answer = -1;
+
+        return answer;
+    }
+
+}
