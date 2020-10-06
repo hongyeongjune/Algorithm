34a367dcdf8af936b15142e39780b678f263f15f
hongyeongjune <hongyeongjune@naver.com>
2020-10-06 19:43:19 +0900

    월간 코드 챌린지 시즌1 / 풍선터뜨리기 / 프로그래머스 / Level3

---
+++ src/월간코드챌린지/시즌1/풍선터뜨리기/Solution.java
@@ -0,0 +1,22 @@
+package 월간코드챌린지.시즌1.풍선터뜨리기;
+
+import java.util.HashSet;
+
+public class Solution {
+    public int solution(int[] a) {
+        HashSet<Integer> hashSet = new HashSet<>();
+        int min = a[0];
+        for (int i = 1; i < a.length; i++) {
+            hashSet.add(min);
+            min = Math.min(a[i], min);
+        }
+
+        min = a[a.length - 1];
+        for (int i = a.length - 2; i >= 0; i--) {
+            hashSet.add(min);
+            min = Math.min(a[i], min);
+        }
+
+        return hashSet.size();
+    }
+}
