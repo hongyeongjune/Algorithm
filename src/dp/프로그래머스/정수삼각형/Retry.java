9aeb920629524dfb26985bee32e7d5bfdd0ad206
hongyeongjune <hongyeongjune@naver.com>
2020-10-27 17:00:11 +0900

    다시풀어보기 / DP / 프로그래머스 고득점 킷 / Level3

---
+++ src/dp/프로그래머스/정수삼각형/Retry.java
@@ -0,0 +1,26 @@
+package dp.프로그래머스.정수삼각형;
+
+import java.util.Arrays;
+
+public class Retry {
+    public static int solution(int[][] triangle) {
+        for (int i = 1; i < triangle.length; i++) {
+            for (int j = 0; j < triangle[i].length; j++) {
+                if (j != 0 && j != i) {
+                    triangle[i][j] +=
+                            Math.max(triangle[i - 1][j - 1], triangle[i - 1][j]);
+                } else if (j == 0 && j != i) {
+                    triangle[i][j] += triangle[i - 1][j];
+                } else if (j != 0 && j == i) {
+                    triangle[i][j] += triangle[i - 1][j - 1];
+                }
+            }
+        }
+
+        Arrays.sort(triangle[triangle.length - 1]);
+
+
+        return triangle[triangle.length - 1][triangle[triangle.length - 1].length - 1];
+    }
+
+}
