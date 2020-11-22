1b0d23b306c58017e87997b3041402b11a043d0b
hongyeongjune <hongyeongjune@naver.com>
2020-11-22 14:07:01 +0900

    연습 문제 / 프로그래머스 모든 문제 / Level1

---
+++ src/연습문제/두정수사이의합/Solution.java
@@ -0,0 +1,15 @@
+package 연습문제.두정수사이의합;
+
+public class Solution {
+    public long solution(int a, int b) {
+        long answer = 0;
+
+        if (a == b) return a;
+
+        for (int i = Math.min(a, b); i <= Math.max(a, b); i++) {
+            answer += i;
+        }
+
+        return answer;
+    }
+}
