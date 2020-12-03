6c20e90cc4112866902247e5d33e7de11a97dc1d
hongyeongjune <hongyeongjune@naver.com>
2020-12-03 14:12:45 +0900

    연습문제 / 프로그래머스 모든 문제 / Level1

---
+++ src/연습문제/문자열내p외y의개수/Solution.java
@@ -0,0 +1,18 @@
+package 연습문제.문자열내p외y의개수;
+
+public class Solution {
+    public static boolean solution(String s) {
+        boolean answer = true;
+        int[] count = new int[2];
+        s = s.toUpperCase();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'P') count[0]++;
+            else if (s.charAt(i) == 'Y') count[1]++;
+        }
+
+        if (count[0] != count[1]) return answer = false;
+
+        return answer;
+    }
+}
