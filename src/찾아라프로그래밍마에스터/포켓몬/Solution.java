4c7fc6ee3ed0ac230729638dceb2f9e3e2a63566
hongyeongjune <hongyeongjune@naver.com>
2020-12-04 14:37:34 +0900

    찾아라 프로그래밍 마에스터 / 프로그래머스 모든 문제 / Level2

---
+++ src/찾아라프로그래밍마에스터/포켓몬/Solution.java
@@ -0,0 +1,21 @@
+package 찾아라프로그래밍마에스터.포켓몬;
+
+import java.util.HashMap;
+
+public class Solution {
+    public static int solution(int[] nums) {
+
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+        for (int num : nums) {
+            if (hashMap.get(num) == null) {
+                hashMap.put(num, 1);
+            } else hashMap.put(num, hashMap.get(num) + 1);
+        }
+
+        if (nums.length / 2 <= hashMap.size()) return nums.length / 2;
+        else return hashMap.size();
+
+    }
+
+}
