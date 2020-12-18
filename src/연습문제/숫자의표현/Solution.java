416da1f2d2897cf44037af96bdf1fefd7214cbf1
hongyeongjune <hongyeongjune@naver.com>
2020-12-18 22:29:44 +0900

    연습 문제 / 프로그래머스 모든 문제 / Level2

---
+++ src/연습문제/숫자의표현/Solution.java
@@ -0,0 +1,27 @@
+package 연습문제.숫자의표현;
+
+public class Solution {
+    public static int solution(int n) {
+        int answer = 0;
+
+        for (int i = 0; i <= (n / 2); i++) {
+            if (count(i, n)) answer++;
+        }
+
+        return answer + 1;
+    }
+
+    public static boolean count(int index, int n) {
+
+        int sum = 0;
+
+        do {
+            index++;
+            sum += index;
+        } while (sum < n);
+
+        if (sum == n) return true;
+        else return false;
+
+    }
+}
