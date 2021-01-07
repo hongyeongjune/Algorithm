c1ea047ee549f7ee71bd7575f871196339614b83
hongyeongjune <hongyeongjune@naver.com>
2021-01-07 09:41:24 +0900

    LeetCode 53 MaximumSubarray solved Problems

---
+++ src/알고리즘스터디/리트코드/LC_53_MaximumSubarray/Solution.java
@@ -0,0 +1,16 @@
+package 알고리즘스터디.리트코드.LC_53_MaximumSubarray;
+
+public class Solution {
+    public int maxSubArray(int[] nums) {
+
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int answer = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            answer = Math.max(answer, dp[i]);
+        }
+
+        return answer;
+    }
+}
