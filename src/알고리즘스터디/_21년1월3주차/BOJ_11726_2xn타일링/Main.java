8cd5b187ea174c1e8af883ef2b79b602a7fa4069
hongyeongjune <hongyeongjune@naver.com>
2021-01-12 20:42:41 +0900

    백준 11726 2xn타일링 solved problem

---
+++ src/알고리즘스터디/_21년1월3주차/BOJ_11726_2xn타일링/Main.java
@@ -0,0 +1,26 @@
+package 알고리즘스터디._21년1월3주차.BOJ_11726_2xn타일링;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static int[] dp = new int[1001];
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(recursion(n));
+
+    }
+
+    public static int recursion(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        if (dp[n] != 0) return dp[n];
+        return dp[n] = (recursion(n - 1) + recursion(n - 2)) % 10007;
+    }
+}
