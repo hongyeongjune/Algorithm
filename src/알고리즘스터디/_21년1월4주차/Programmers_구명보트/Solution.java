6da26437df52425c9a14c2da52f45b5b920636f4
hongyeongjune <hongyeongjune@naver.com>
2021-01-18 19:28:35 +0900

    프로그래머스 구명보트 solved problem

---
+++ src/알고리즘스터디/_21년1월4주차/Programmers_구명보트/Solution.java
@@ -0,0 +1,23 @@
+package 알고리즘스터디._21년1월4주차.Programmers_구명보트;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static int solution(int[] people, int limit) {
+        Arrays.sort(people);
+
+        int left = 0;
+        int right = people.length - 1;
+        int answer = 0;
+
+        while (left <= right) {
+            // 무조건 2개 씩 담을 수 있으므로 가장 큰 값과 작은 값을 합치면 오른쪽으로 이동
+            if (people[left] + people[right] <= limit) left++;
+            // 2개씩 못 담으면 왼쪽으로 이동하고 값을 하나 늘림
+            right--;
+            answer++;
+        }
+
+        return answer;
+    }
+}
