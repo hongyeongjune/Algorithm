3504d34867ce72ab04f6bb0e15d205c5740346fb
hongyeongjune <hongyeongjune@naver.com>
2021-03-30 09:01:07 +0900

    solved problem LeetCode 1641 Count Sorted Vowel Strings

---
+++ src/리트코드/LeetCode_1641_CountSortedVowelStrings/Solution.java
@@ -0,0 +1,22 @@
+package 리트코드.LeetCode_1641_CountSortedVowelStrings;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int countVowelStrings(int n) {
+        int[][] memo = new int[n + 1][5];
+
+        Arrays.fill(memo[1], 1);
+
+        for (int i = 2; i <= n; i++) {
+            memo[i][0] = 1;
+            for (int j = 1; j < 5; j++) {
+                int sum = 0;
+                for (int k = 0; k <= j; k++) sum += memo[i - 1][k];
+                memo[i][j] = sum;
+            }
+        }
+
+        return Arrays.stream(memo[n]).sum();
+    }
+}
