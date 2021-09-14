b914d7d3f2b1843c3ef9ab90242611f32f6031d0
hongyeongjune <hongyeongjune@naver.com>
2021-09-14 13:44:47 +0900

    solved problem

---
+++ src/프로그래머스/Programmers_행렬의곱셈/Solution.java
@@ -0,0 +1,26 @@
+package 프로그래머스.Programmers_행렬의곱셈;
+
+public class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
+
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2[0].length; j++) {
+                for (int k = 0; k < arr1[i].length; k++) {
+                    answer[i][j] += (arr1[i][k] * arr2[k][j]);
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.solution(new int[][]{
+                {1, 4}, {3, 2}, {4, 1}
+        }, new int[][]{
+                {3, 3}, {3, 3}
+        });
+    }
+}
