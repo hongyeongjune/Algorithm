d648a61bb354978673b4e148b0a05616283c5832
hongyeongjune <hongyeongjune@naver.com>
2021-10-12 11:31:37 +0900

    solved problem

---
+++ src/리트코드/LeetCode_397_IntegerReplacement/Solution.java
@@ -0,0 +1,16 @@
+package 리트코드.LeetCode_397_IntegerReplacement;
+
+public class Solution {
+    public int integerReplacement(int n) {
+        if (n == Integer.MAX_VALUE) return 32;
+        int answer = 0;
+        while (n > 1) {
+            if (n % 2 == 0) n /= 2;
+            else if ((n + 1) % 4 == 0 && (n - 1) != 2) n++;
+            else n--;
+
+            answer++;
+        }
+        return answer;
+    }
+}
